38bd6288d70ce858de23b0802af78ab0dea0f7ab
Dylan Barbona <dylanbarbona97@gmail.com>
2022-09-26 14:32:50 -0300

    Prometheus & Grafana

--- terraform/module/scripts/init-bastion.sh
+++ terraform/module/scripts/init-bastion.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
+
+#############################################################
+# Instalación de dependencias
+#############################################################
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
   stable" -y
 sudo sysctl -p
 sudo apt-get update &&
-sudo apt-get install -y docker-ce apt-transport-https curl ansible -y &&
+sudo apt-get install -y docker-ce docker-compose apt-transport-https curl ansible -y &&
 sudo usermod -aG docker ubuntu &&
 sudo systemctl restart docker &&
 sudo systemctl enable docker.service &&
 sudo systemctl daemon-reload
-
 sudo apt-get install python3-pip -y
 sudo pip install boto3
-sudo mkdir -p /opt-ansible/inventory
+
+#############################################################
+# Ansiible
+#############################################################
+
 sudo mkdir /etc/ansible
 
 sudo mkdir -p /opt/ansible/inventory
@@ -40,6 +48,52 @@ aws_access_key: ${AWS_ACCESS_KEY_ID}
 aws_secret_key: ${AWS_SECRET_ACCESS_KEY}
 keyed_groups:
   - key: tags.Name" > /opt/ansible/inventory/aws_ec2.yaml
+
+#############################################################
+# PROMETHEUS & GRAFANA (Instalación)
+#############################################################
+
+sudo touch ~/docker-compose.yml
+sudo chmod 777 ~/docker-compose.yml
+sudo echo "
+version: '3'
+
+services:
+  grafana:
+    image: grafana/grafana-enterprise:latest
+    container_name: grafana
+    ports:
+      - 3000:3000
+    networks:
+      - metrics_net
+    volumes:
+      - grafana_data:/var/lib/grafana
+      - ./grafana/provisioning:/etc/grafana/provisioning
+
+  prometheus:
+    image: bitnami/prometheus:latest
+    container_name: prometheus
+    restart: always
+    ports:
+      - 9090:9090
+    volumes:
+      - ~/prometheus/conf/prometheus.yml:/opt/bitnami/prometheus/conf/prometheus.yml:ro
+      - prometheus_data:/opt/bitnami/prometheus/data
+    networks:
+      - metrics_net
+
+volumes:
+  prometheus_data:
+  grafana_data:
+
+networks:
+  metrics_net:
+" > ~/docker-compose.yml
+
+
+#############################################################
+# PROMETHEUS & GRAFANA (Configuraciones)
+#############################################################
 
 sudo mkdir -p ~/prometheus/conf
 sudo touch ~/prometheus/conf/prometheus.yml
@@ -71,4 +125,61 @@ scrape_configs:
 #        target_label: instance
 " > ~/prometheus/conf/prometheus.yml
 
-sudo docker run -p9090:9090 -d --volume "$(pwd)/prometheus/conf/prometheus.yml:/opt/bitnami/prometheus/conf/prometheus.yml:ro" --volume "prometheus_data:/opt/bitnami/prometheus/data" --name prometheus bitnami/prometheus:latest
+sudo mkdir -p ~/grafana/provisioning/datasources
+sudo touch ~/grafana/provisioning/datasources/default.yml
+sudo chmod 777 -R ~/grafana/provisioning/datasources/default.yml
+
+sudo echo "
+apiVersion: 1
+
+deleteDatasources:
+  - name: Prometheus
+    orgId: 1
+
+datasources:
+- name: Prometheus
+  type: prometheus
+  access:
+  url: http://prometheus:9090
+  password:
+  user:
+  database:
+  basicAuth: false
+  basicAuthUser:
+  basicAuthPassword:
+  withCredentials:
+  isDefault: true
+  jsonData:
+     tlsAuth: false
+     tlsAuthWithCACert: false
+  secureJsonData:
+    tlsCACert: ""
+    tlsClientCert: ""
+    tlsClientKey: ""
+  version: 1
+  editable: true
+" > ~/grafana/provisioning/datasources/default.yml
+
+# Configuración Dashboard
+sudo touch ~/grafana/provisioning/dashboards/node-exporter-full.json
+sudo chmod 777 ~/grafana/provisioning/dashboards/node-exporter-full.json
+sudo curl https://raw.githubusercontent.com/rfmoz/grafana-dashboards/master/prometheus/node-exporter-full.json > ~/grafana/provisioning/dashboards/node-exporter-full.json
+
+sudo mkdir -p ~/grafana/provisioning/dashboards
+sudo touch ~/grafana/provisioning/dashboards/default.yml
+sudo chmod 777 -R ~/grafana/provisioning/dashboards/default.yml
+
+sudo echo "
+apiVersion: 1
+providers:
+- name: 'Prometheus'
+  orgId: 1
+  folder: ''
+  type: file
+  disableDeletion: false
+  editable: true
+  options:
+    path: /etc/grafana/provisioning/dashboards
+" > ~/grafana/provisioning/dashboards/default.yml
+
+sudo docker-compose up -d
